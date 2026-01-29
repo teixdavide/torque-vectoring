3cc6d4768f86abc7354468812e245bd4ba5756d5
Davide64-dev <davidpcu@hotmail.com>
2026-01-29 17:10:48 +0000

    feat: added car parameters data structure

---
+++ torque-vectoring/include/data_structures/car_parameters.hpp
@@ -0,0 +1,29 @@
+struct CarParameters {
+  double front_axle_distance;
+  double rear_axle_distance;
+  double front_track_width;
+  double rear_track_width;
+  double center_of_gravity_height;
+  double mass;
+  double moment_of_inertia_z;
+  double wheel_radius;
+  double gear_ratio;
+  double inner_steering_ratio;
+  double outer_steering_ratio;
+  double cornering_stiffness_front;
+  double cornering_stiffness_rear;
+
+  struct TireParameters {
+    double lateral_stiffness_factor;
+    double shape_factor;
+    double peak_factor;
+    double curvature_factor;
+  } tire_parameters;
+
+  struct AerodynamicsParameters {
+    double drag_coefficient;
+    double frontal_area;
+    double lift_coefficient;
+    double center_of_pressure_distribution;
+  } aerodynamics_parameters;
+};
--- torque-vectoring/include/data_structures/vehicle_state.hpp
+++ torque-vectoring/include/data_structures/vehicle_state.hpp
@@ -1,5 +1,6 @@
 struct VehicleState {
   double yaw_rate;
+  double sideslip_angle;
   double velocity_x;
   double velocity_y;
   double acceleration_x;
